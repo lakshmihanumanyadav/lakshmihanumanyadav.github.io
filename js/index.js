002ba2f3419f31244884d8ea5b843401e257a444
lakshmihanumanyadav <theanuragkumarofficial@gmail.com>
2023-02-25 02:29:52 +0530

    Journey Added

--- js/index.js
+++ js/index.js
@@ -1,8 +1,19 @@
+
+
 function scrollToPosition(position){
     var scrollDiv = document.getElementById(position).offsetTop;
     window.scrollTo({ top: scrollDiv, behavior: 'smooth' });
 }
 
-// function changeTitle(message){
-//     document.getElementById("card_title").innerHTML = message
-// }
+function callCard(){
+    const triggerBottom = window.innerHeight/5 * 4
+    cards.forEach(card => {
+        // The getBoundingClientRect() method returns the size of an element and its position relative to the viewport
+        const cardTop = card.getBoundingClientRect().top
+        if(cardTop < triggerBottom){
+            card.classList.add('show')
+        } else{
+            card.classList.remove('show')
+        }
+    })
+}
